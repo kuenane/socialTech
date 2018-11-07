20bf99956e2cbed71f6cd0c8e5b7889e4767fd04
omeyer <meyeroliver9401@gmail.com>
2018-11-07 03:51:20 +0200

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import "./components/AppNavbar";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+//import AppNavbar from "./components/AppNavbar";
+import ButtonAppBar from "./components/ButtonAppBar";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MuiThemeProvider>
+        <ButtonAppBar />
+      </MuiThemeProvider>
     );
   }
 }
