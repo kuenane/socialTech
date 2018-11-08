5d145ead9438ab0d12d3136510e42cfd521e8ec7
omeyer <meyeroliver9401@gmail.com>
2018-11-08 03:04:27 +0200

    Updated the Registration. it now has a label that toggles between user and company

--- client/src/components/content/RightPane.js
+++ client/src/components/content/RightPane.js
@@ -1,18 +1,98 @@
-import React from "react";
-import { Paper, Typography, Avatar, Button } from "@material-ui/core";
+import React, { Component } from "react";
+import {
+  Paper,
+  Typography,
+  Avatar,
+  Button,
+  FormGroup
+} from "@material-ui/core";
 import FormControl from "@material-ui/core/FormControl";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Input from "@material-ui/core/Input";
 import InputLabel from "@material-ui/core/InputLabel";
 import Checkbox from "@material-ui/core/Checkbox";
+import Switch from "@material-ui/core/Switch";
 
-export default ({ style }) => (
+//import SwitchLabel from "./SwitchLabel";
+
+class RightPane extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = {
+      checkedA: false
+    };
+  }
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.checked });
+  };
+
+  toggleLabel() {
+    return this.state.checkedA === false ? "User" : "Company";
+  }
+
+  render() {
+    return (
+      <Paper style={this.props.style.Paper}>
+        <FormGroup>
+          <Typography component="h1" variant="h5">
+            Registration
+          </Typography>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={this.state.checkedA}
+                onChange={this.handleChange("checkedA")}
+                value="checkedB"
+                color="primary"
+              />
+            }
+            label={this.toggleLabel()}
+          />
+
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel>Username</InputLabel>
+            <Input id="username" autoFocus />
+          </FormControl>
+
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="email">Email Address</InputLabel>
+            <Input id="email" name="email" autoComplete="email" autoFocus />
+          </FormControl>
+
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <Input
+              name="password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+          </FormControl>
+          <Button type="submit" fullWidth variant="contained" color="primary">
+            Register
+          </Button>
+        </FormGroup>
+      </Paper>
+    );
+  }
+}
+
+export default RightPane;
+
+/*export default ({ style }) => (
   <Paper style={style.Paper}>
     <Typography component="h1" variant="h5">
       Registration
     </Typography>
 
     <form className={style.form}>
+      <FormControlLabel
+        control={<Switch checked={false} onChange={false} value={false} />}
+        label="Private User/ Company"
+      />
+
       <FormControl margin="normal" required fullWidth>
         <InputLabel>Username</InputLabel>
         <Input id="username" autoFocus />
@@ -44,8 +124,8 @@ export default ({ style }) => (
         color="primary"
         className={style.submit}
       >
-        Sign in
+        Register
       </Button>
     </form>
   </Paper>
-);
+);*/
