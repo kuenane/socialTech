8bd203f8e7922863961321216e8a5dc5d968f1e0
Oliver Reginald MEYER <omeyer@c3r5s1.wethinkcode.co.za>
2018-11-09 10:58:43 +0200

    added responsiveness on the layout (note theres is more todo on this topic)

--- client/src/components/content/index.js
+++ client/src/components/content/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 import RightPane from "./RightPane";
 
+const style = {
+  Paper: { padding: 50, marginTop: 10, marigBottom: 10 }
+};
+
 export default props => (
-  <Grid container sm={12} justify="center">
-    <Grid sm={6}>
+  <Grid container justify="center">
+    <Grid xs={12} sm={6}>
       <RightPane style={style} />
     </Grid>
   </Grid>
