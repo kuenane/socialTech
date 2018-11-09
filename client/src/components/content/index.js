019c010388b396c9e0a453758e6d153e052c6e3c
Oliver Reginald MEYER <omeyer@c3r5s1.wethinkcode.co.za>
2018-11-09 10:48:26 +0200

    removed the WithStylesimport

--- client/src/components/content/index.js
+++ client/src/components/content/index.js
@@ -1,11 +1,6 @@
 import React from "react";
-import { Grid, Paper } from "@material-ui/core";
-import LeftPane from "./LeftPane";
+import { Grid } from "@material-ui/core";
 import RightPane from "./RightPane";
-
-const style = {
-  Paper: { padding: 50, marginTop: 10, marigBottom: 10 }
-};
 
 export default props => (
   <Grid container sm={12} justify="center">
