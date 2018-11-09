bbfd1a4c00006ab53854af4fece170933841a631
Oliver Reginald MEYER <omeyer@c3r5s1.wethinkcode.co.za>
2018-11-09 10:08:30 +0200

    moved registration to the center for now, this layout will changed later

--- client/src/components/content/index.js
+++ client/src/components/content/index.js
@@ -4,15 +4,12 @@ import LeftPane from "./LeftPane";
 import RightPane from "./RightPane";
 
 const style = {
-  Paper: { padding: 20, marginTop: 10, marigBottom: 10 }
+  Paper: { padding: 50, marginTop: 10, marigBottom: 10 }
 };
 
 export default props => (
-  <Grid container sm={12}>
-    <Grid item sm={6}>
-      <LeftPane style={style} />
-    </Grid>
-    <Grid item sm={6}>
+  <Grid container sm={12} justify="center">
+    <Grid sm={6}>
       <RightPane style={style} />
     </Grid>
   </Grid>
