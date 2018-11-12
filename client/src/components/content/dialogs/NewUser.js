ba881fd20ab87e9c3ea0c305fc868577e5e5c69d
meyeroliver <omeyer@student.wethinkcode.co.za>
2018-11-12 10:50:56 +0200

    refactoring

--- client/src/components/content/dialogs/NewUser.js
+++ client/src/components/content/dialogs/NewUser.js
@@ -58,31 +58,3 @@ class NewUser extends Component {
 }
 
 export default NewUser;
-
-/**<Fragment>
-                <Button onClick={this.handleClickOpen}>Register</Button>
-        <Dialog
-          open={this.state.open}
-          onClose={this.handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">
-            {"Use Google's location service?"}
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              Let Google help apps determine location. This means sending
-              anonymous location data to Google, even when no apps are running.
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleClose} color="primary">
-              Disagree
-            </Button>
-            <Button onClick={this.handleClose} color="primary" autoFocus>
-              Agree
-            </Button>
-          </DialogActions>
-        </Dialog>
-        </Fragment> */
