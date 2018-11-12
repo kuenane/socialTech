183d7c2762c4018b3267aa9ce4d530d8b18a911c
meyeroliver <omeyer@student.wethinkcode.co.za>
2018-11-12 11:11:12 +0200

    now takes in a props of toggle and create a json object fo the user to be posted to the db

--- client/src/components/content/dialogs/NewUser.js
+++ client/src/components/content/dialogs/NewUser.js
@@ -9,17 +9,45 @@ import {
 } from "@material-ui/core";
 
 class NewUser extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.registerUser = this.registerUser.bind(this);
+  }
+
   state = {
     open: false
   };
 
   handleClickOpen = () => {
     this.setState({ open: true });
+    this.registerUser();
   };
 
   handleClose = () => {
     this.setState({ open: false });
   };
+
+  registerUser() {
+    var userInput = {};
+    if (this.props.toggle === false) {
+      userInput = {
+        Firstname: document.getElementById("firstname").value,
+        Lastname: document.getElementById("lastname").value,
+        Username: document.getElementById("username").value,
+        Email: document.getElementById("email").value,
+        Password: document.getElementById("password").value
+      };
+    } else {
+      userInput = {
+        CompanyName: document.getElementById("companyName").value,
+        Username: document.getElementById("username").value,
+        Email: document.getElementById("email").value,
+        Password: document.getElementById("password").value
+      };
+    }
+    console.log(userInput);
+  }
 
   render() {
     return (
