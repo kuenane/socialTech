9f566468d10d60293c078b2a31324ffd52a29fbb
meyeroliver <omeyer@student.wethinkcode.co.za>
2018-11-12 11:15:17 +0200

    made inner grid an item

--- client/src/components/content/index.js
+++ client/src/components/content/index.js
@@ -2,14 +2,10 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 import RightPane from "./RightPane";
 
-const style = {
-  Paper: { padding: 50, marginTop: 10, marigBottom: 10 }
-};
-
 export default props => (
   <Grid container justify="center">
-    <Grid xs={12} sm={6} md={4}>
-      <RightPane style={style} />
+    <Grid item xs={12} sm={6} md={4}>
+      <RightPane />
     </Grid>
   </Grid>
 );
