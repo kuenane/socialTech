976a61c3e1fb7818b9762ab3c3d06d72261ad184
Oliver Reginald MEYER <omeyer@c3r5s1.wethinkcode.co.za>
2018-11-12 09:30:55 +0200

    introduced a MuiThemProvider for the general styling of the app

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const theme = createMuiTheme({
+  overrides: {
+    Header: {
+      root: {
+        flexGrow: 1
+      },
+      grow: {
+        flexGrow: 1
+      },
+      menuButton: {
+        marginLeft: -12,
+        marginRight: 20
+      }
+    }
+  }
+});
+
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
