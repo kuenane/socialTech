e10979d4f7d75bfe8a8e82222781e2a4e7f11c61
meyeroliver <omeyer@student.wethinkcode.co.za>
2018-11-13 10:17:29 +0200

    started a basic styling on the header

--- client/src/components/layouts/Header.js
+++ client/src/components/layouts/Header.js
@@ -1,18 +1,48 @@
 import React, { Component, Fragment } from "react";
 import { AppBar, Toolbar, Typography, Button } from "@material-ui/core";
 import IconButton from "@material-ui/core/IconButton";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = {
+  headerText: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
+};
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.classes = props;
+  }
+
   render() {
+    const { classes } = this.classes;
     return (
       <Fragment>
-        <AppBar position="static">
+        <AppBar position="static" color="primary">
           <Toolbar>
-            <IconButton color="inherit" aria-label="Menu" />
-            <Typography variant="h6" color="inherit">
-              Stationary 4 Africa
+            <IconButton
+              color="inherit"
+              aria-label="Menu"
+              classes={{ root: classes.menuButton }}
+            />
+            <Typography
+              variant="h6"
+              color="inherit"
+              classes={{
+                root: classes.headerText
+              }}
+            >
+              S4A
             </Typography>
-            <Button color="inherit">Login</Button>
+            <Button variant="contained" color="primary">
+              Login
+            </Button>
           </Toolbar>
         </AppBar>
       </Fragment>
@@ -20,4 +50,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+Header.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Header);
