638477613ad3bfcc650ce8dca5e3487289b460b9
meyeroliver <omeyer@student.wethinkcode.co.za>
2018-11-13 15:42:05 +0200

    applied a basic style for now

---
+++ client/src/components/content/Body.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { Grid } from "@material-ui/core";
+import RightPane from "./RightPane";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = {
+  bodyStyle: {
+    height: "calc(100% - 64px - 48px)"
+  }
+};
+
+class Body extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <Grid container={true} justify="center" className={classes.bodyStyle}>
+        <Grid item xs={12} sm={6}>
+          <RightPane />
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+Body.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Body);
