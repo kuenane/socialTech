d0dc00332449bf7b9e2daea455fd8793c185980f
meyeroliver <omeyer@student.wethinkcode.co.za>
2018-11-15 12:54:31 +0200

    changed to class, also lifted its state to Parent

--- client/src/components/content/formInputs/Username.js
+++ client/src/components/content/formInputs/Username.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import Input from "@material-ui/core/Input";
 import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
 
-export default props => (
-  <FormControl margin="normal" required>
-    <InputLabel>usertname</InputLabel>
-    <Input id="username" />
-  </FormControl>
-);
+class Username extends Component {
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  /*showValidarionErrors(element, message) {
+    this.props.prevState = {
+      errors: [...this.props.prevState.errors, { element, message }]
+    };
+  }
+
+  clearValidationErrors(element) {
+    let newArr = [];
+    for (let error of this.props.prevState.errors) {
+      if (element != error.element) {
+        newArr.push(error);
+      }
+    }
+    return { errors: newArr };
+  }*/
+
+  onChange(event) {
+    //this.props.username({ username: event.target.value });
+    this.props.onUsernameChange(event.target.value);
+  }
+
+  render() {
+    const username = this.props.username;
+    console.log(username);
+    return (
+      <FormControl margin="normal" required>
+        <InputLabel>username</InputLabel>
+        <Input id="username" value={username} onChange={this.onChange} />
+      </FormControl>
+    );
+  }
+}
+
+export default Username;
