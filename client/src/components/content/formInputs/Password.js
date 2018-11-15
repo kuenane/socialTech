43d0464b059a59eefd7c39b4f7274781f2ba1464
meyeroliver <omeyer@student.wethinkcode.co.za>
2018-11-15 13:30:08 +0200

    changed to class, also lifted its state to Parent

--- client/src/components/content/formInputs/Password.js
+++ client/src/components/content/formInputs/Password.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import Input from "@material-ui/core/Input";
 import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
 
-export default props => (
-  <FormControl margin="normal" required>
-    <InputLabel htmlFor="password">Password</InputLabel>
-    <Input
-      name="password"
-      type="password"
-      id="password"
-      autoComplete="current-password"
-    />
-  </FormControl>
-);
+class Password extends Component {
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(event) {
+    this.props.onPasswordChange(event.target.value);
+  }
+
+  render() {
+    const password = this.props.password;
+
+    return (
+      <FormControl margin="normal" required>
+        <InputLabel htmlFor="password">Password</InputLabel>
+        <Input
+          id="password"
+          name="password"
+          value={password}
+          onChange={this.onChange}
+          type="password"
+        />
+      </FormControl>
+    );
+  }
+}
+
+export default Password;
