f20aa185d5fff02719c91d909e12d47945023401
meyeroliver <omeyer@student.wethinkcode.co.za>
2018-11-15 13:46:35 +0200

    changed to class, also lifted its state to Parent

--- client/src/components/content/formInputs/Firstname.js
+++ client/src/components/content/formInputs/Firstname.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import Input from "@material-ui/core/Input";
 import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
 
-export default props => (
-  <FormControl margin="normal" required>
-    <InputLabel>Firstname</InputLabel>
-    <Input id="firstname" autoFocus />
-  </FormControl>
-);
+class Firstname extends Component {
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(event) {
+    this.props.onFirstnameChange(event.target.value);
+  }
+
+  render() {
+    const firstname = this.props.firstname;
+    return (
+      <FormControl margin="normal" required>
+        <InputLabel htmlFor="firstname">Firstname</InputLabel>
+        <Input
+          id="firstname"
+          name="firstname"
+          value={firstname}
+          onChange={this.onChange}
+        />
+      </FormControl>
+    );
+  }
+}
+
+export default Firstname;
