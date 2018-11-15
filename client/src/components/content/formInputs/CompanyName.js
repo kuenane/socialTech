1beba1567dee729f0d46faec7b8a0f1dfec7095c
meyeroliver <omeyer@student.wethinkcode.co.za>
2018-11-15 23:55:32 +0200

    display error message when needed and also clears. created sort function for when more intense validation is required

--- client/src/components/content/formInputs/CompanyName.js
+++ client/src/components/content/formInputs/CompanyName.js
@@ -1,20 +1,36 @@
 import React, { Component } from "react";
-import Input from "@material-ui/core/Input";
-import InputLabel from "@material-ui/core/InputLabel";
-import FormControl from "@material-ui/core/FormControl";
+import {
+  Input,
+  InputLabel,
+  FormControl,
+  FormHelperText
+} from "@material-ui/core";
 
 class CompanyName extends Component {
   constructor(props) {
     super(props);
     this.onChange = this.onChange.bind(this);
+    this.sortErrors = this.sortErrors.bind(this);
   }
 
   onChange(event) {
     this.props.onCompanyNameChange(event.target.value);
+    this.props.onClearErrorMsg("companyName");
+  }
+
+  sortErrors(errors, key) {
+    let newArr = [];
+    for (let error of errors) {
+      if (key === error.element) {
+        newArr.push(error.message);
+      }
+    }
+    return newArr;
   }
 
   render() {
-    const companyName = this.props.companyName;
+    const companyName = this.props.parentState.companyName;
+    const errors = this.props.parentState.errors;
 
     return (
       <FormControl margin="normal" required>
@@ -25,6 +41,9 @@ class CompanyName extends Component {
           value={companyName}
           onChange={this.onChange}
         />
+        <FormHelperText id="companyNameHelper" error={true}>
+          {this.sortErrors(errors, "username")[0]}
+        </FormHelperText>
       </FormControl>
     );
   }
