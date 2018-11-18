2e126a37bfc4cbf3f89de53edf3c0cf73ade819b
omeyer <meyeroliver9401@gmail.com>
2018-11-18 19:23:21 +0200

    set up the body mooi so that the login can be plugged in easy

--- client/src/components/layouts/Body.js
+++ client/src/components/layouts/Body.js
@@ -1,22 +1,64 @@
-import React, { Component } from "react";
-import { Grid } from "@material-ui/core";
-import RightPane from "../content/RightPane";
+import React, { Component, Fragment } from "react";
+import { Grid, Paper, Tabs, Tab } from "@material-ui/core";
+import Register from "../content/Register";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 
 const styles = {
   bodyStyle: {
-    height: "calc(100% - 64px - 48px)"
+    height: "calc(100% - 59px - 48px)"
+  },
+  tab: {
+    width: "50%"
+  },
+  bodyHeader: {
+    margin: "50px 5px 0px 5px"
   }
 };
 
 class Body extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.displayChange = this.displayChange.bind(this);
+  }
+
+  handleChange(event, value) {
+    this.setState({ value });
+  }
+
+  displayChange() {
+    const value = this.state.value;
+    if (value === 0) {
+    } else {
+      return <Register />;
+    }
+  }
+
   render() {
     const { classes } = this.props;
+    const value = this.state.value;
     return (
       <Grid container={true} justify="center" className={classes.bodyStyle}>
         <Grid item xs={12} sm={6}>
-          <RightPane />
+          <Fragment>
+            <Paper className={classes.bodyHeader}>
+              <Tabs
+                value={value}
+                indicatorColor="secondary"
+                textColor="primary"
+                centered
+                onChange={this.handleChange}
+              >
+                <Tab label="Login" className={classes.tab} />
+                <Tab label="Register" className={classes.tab} />
+              </Tabs>
+            </Paper>
+            {this.displayChange()}
+          </Fragment>
         </Grid>
       </Grid>
     );
