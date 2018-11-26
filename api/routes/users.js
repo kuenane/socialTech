0efb0aa6c13a4b805d6e5795f899fb3df6af5ec4
omeyer <meyeroliver9401@gmail.com>
2018-11-26 03:02:30 +0200

    played around to check how this works

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const UserController = require('../controllers/users')
+const UserController = require("../controllers/users");
 
 //create a user
-router.post("/signup", UserController.user_signUp_user)
+router.post("/signup", UserController.user_signUp_user);
 //login users
-router.post("/login", UserController.user_login_user)
+router.post("/login", UserController.user_login_user);
 
-router.get('/', UserController.user_get_all)
+router.get("/", UserController.user_get_all);
 //delete a user
-router.delete("/:userId", UserController.user_del_user)
+router.delete("/:userId", UserController.user_del_user);
 
-module.exports = router
+module.exports = router;
